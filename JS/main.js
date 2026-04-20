6656063b00aabad148bfacd1ffbddf1b12d8e4fb
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-20 19:20:46 +0600

    I'm learning JS

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+// document.write("JavaScript говорит привет!");
+//  console.log("JavaScript говорит привет!");
+//  console.info("JavaScript говорит привет!");
+// console.error("JavaScript говорит привет!");
+// console.warn("JavaScript говорит привет!");
+
+// 1.  Переменные создание!!!!!!
+
+// var num;
+// num = 5;
+
+// var num = 5;
+// num = 7;
+// console.log("Переменная: " + num + ".");
+
+// const num = 5;
+// // num = 7;
+// console.log("Переменная: " + num + ".");
+
+// var number;
+// number = true;
+
+// var num__1 = 5; число
+// var num__1 = "5"; строка
+
+// 2.  Математические действия!!!
+
+var num_1 = 5;
+var num_2 = 15;
+
+// console.log("Вычетание: " + (num_1 - num_2));
+// console.log("Результат: " + (num_1 + num_2));
+// console.log("Умножение: " + num_1 * num_2);
+// console.log("Деление: " + num_1 / num_2);
+// console.log("Проценты: " + (num_1 % num_2));
+
+// var num_3 = 5;
+// num_3 += 7;
+// console.log("Результат:" + num_3);
+
+// var str_1 = Number("12");
+// var str_2 = Number("2");
+
+// console.log("Результат:" + (str_1 + str_2));
+
+console.log("Math:" + Math.PI);
+// console.log("Math:" + Math.min(4, 6, -8, 2, 0, 6)); выводит мин число
+// console.log("Math:" + Math.max(4, 6, 64, 2, 0, 6)); выводит максимальное число
