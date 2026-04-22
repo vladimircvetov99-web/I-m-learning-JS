d40fc68301855cf7a7d4bfaf4d7f5a96f6fd1167
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-22 13:09:17 +0300

    learn styles in JS

--- JS/main.js
+++ JS/main.js
@@ -316,19 +316,24 @@
 
 // 8. События и обработчик событий в JS
 
-var counter = 0;
+// var counter = 0;
 
-function onClickButton(el) {
+// function onClickButton(el) {
 
-    counter++;
-    el.innerHTML = "Вы нажали на кнопку: " + counter;
+//     counter++;
+//     el.innerHTML = "Вы нажали на кнопку: " + counter;
+//     // el.style.background = "red";
+//     // el.style.color = "blue";
 
-    console.log(el.names);
-}
 
-function onInput(el) {
-    if(el.value == "Hello")
-        alert("И тебе привет");
-    console.log(el.value);
-}
+//     el.style.cssText = "border-radius: 5px; border:0, font-size: 20px";
+
+//     console.log(el.names);
+// }
+
+// function onInput(el) {
+//     if(el.value == "Hello")
+//         alert("И тебе привет");
+//     console.log(el.value);
+// }
 
