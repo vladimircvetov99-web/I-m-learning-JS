0fc23c62ae5a432c3bfa772d67d0a637ff04b940
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-22 15:18:33 +0300

    study the event handler

--- JS/main.js
+++ JS/main.js
@@ -337,3 +337,69 @@
 //     console.log(el.value);
 // }
 
+
+
+// 9. Управление HTML и обработка форм при помощи JS
+
+
+// var text = document.getElementById('text');
+// text.title = "New text";
+// console.log(text.title);
+
+// text.style.color = 'red';
+// text.style.backgroundColor = 'blue';
+
+// text.innerHTML = "New<br>string";
+
+// // document.getElementById('text').style.color = "white";
+
+// // var spans = document.getElementsByTagName('span');
+
+// // for(var i = 0; i < spans.length; i++) {
+// //     console.log(spans[i].innerHTML);
+// // }
+// var spans = document.getElementsByClassName('simple-text');
+
+// for(var i = 0; i < spans.length; i++) {
+//     console.log(spans[i].innerHTML);
+// }
+
+
+document.getElementById('main-form').addEventListener("submit", checkForm);
+
+
+
+function checkForm(event) {
+    event.preventDefault();
+    var el = document.getElementById('main-form');
+
+// var name = document.getElementById('name').value; первый способ
+var name = el.name.value;
+var pass = el.pass.value;
+var repass = el.repass.value;
+var state = el.state.value;
+
+var fail = "";
+
+if(name == "" || pass == "" || state == "") 
+    fail = "Заполните все поля";
+    else if(name.length <= 1 || name.length > 50)
+        fail = "Введите корректное имя";
+    else if(pass != repass)
+        fail = "Пароли должны совпадать";
+     else if(pass.split("&").length > 1)
+        fail = "Некоректный пароль"
+
+     if(fail != "") {
+        document.getElementById('error').innerHTML = fail;
+
+     } else {
+        alert("Все данные коректно заполнены");
+        window.location = 'https://vladimircvetov99-web.github.io/2048-game/';
+        
+     }
+
+// console.log(state + "-" + pass + "-" + repass);
+
+}
+
