9f37c9d9dd5d18a25311a6b4b894a2e53ca32a64
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-22 15:19:24 +0300

    study the event handler1

--- JS/main.js
+++ JS/main.js
@@ -365,41 +365,41 @@
 // }
 
 
-document.getElementById('main-form').addEventListener("submit", checkForm);
+// document.getElementById('main-form').addEventListener("submit", checkForm);
 
 
 
-function checkForm(event) {
-    event.preventDefault();
-    var el = document.getElementById('main-form');
+// function checkForm(event) {
+//     event.preventDefault();
+//     var el = document.getElementById('main-form');
 
-// var name = document.getElementById('name').value; первый способ
-var name = el.name.value;
-var pass = el.pass.value;
-var repass = el.repass.value;
-var state = el.state.value;
+// // var name = document.getElementById('name').value; первый способ
+// var name = el.name.value;
+// var pass = el.pass.value;
+// var repass = el.repass.value;
+// var state = el.state.value;
 
-var fail = "";
+// var fail = "";
 
-if(name == "" || pass == "" || state == "") 
-    fail = "Заполните все поля";
-    else if(name.length <= 1 || name.length > 50)
-        fail = "Введите корректное имя";
-    else if(pass != repass)
-        fail = "Пароли должны совпадать";
-     else if(pass.split("&").length > 1)
-        fail = "Некоректный пароль"
+// if(name == "" || pass == "" || state == "") 
+//     fail = "Заполните все поля";
+//     else if(name.length <= 1 || name.length > 50)
+//         fail = "Введите корректное имя";
+//     else if(pass != repass)
+//         fail = "Пароли должны совпадать";
+//      else if(pass.split("&").length > 1)
+//         fail = "Некоректный пароль"
 
-     if(fail != "") {
-        document.getElementById('error').innerHTML = fail;
+//      if(fail != "") {
+//         document.getElementById('error').innerHTML = fail;
 
-     } else {
-        alert("Все данные коректно заполнены");
-        window.location = 'https://vladimircvetov99-web.github.io/2048-game/';
+//      } else {
+//         alert("Все данные коректно заполнены");
+//         window.location = 'https://vladimircvetov99-web.github.io/2048-game/';
         
-     }
+//      }
 
-// console.log(state + "-" + pass + "-" + repass);
+// // console.log(state + "-" + pass + "-" + repass);
 
-}
+// }
 
